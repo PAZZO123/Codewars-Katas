8e33ac02e300e201500766fb5f01a6d97fa36534
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-23 12:13:01 +0200

    23

---
+++ getSum.js
@@ -0,0 +1,22 @@
+function getSum(a, b)
+{
+   var sum=0;
+   //Good luck!
+  //check two numbers if they are equal return one of them
+  //if they are not equal return sum of integers between them
+  if(a===b){
+    return a;
+  }
+  else if(a<b){
+ 
+  for(let i=a;i<=b;i++){
+    sum+=i;
+  }}
+  else{
+    for(let i=a;i>=b;i--){
+      sum+=i;
+    }
+  }
+  return sum;
+}
+console.log(getSum(0,-1))
