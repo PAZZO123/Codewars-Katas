c1841d1468761e0cffd7e7e4cac5940f74ddd30c
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-24 16:33:09 +0200

    24

---
+++ pyramid.js
@@ -0,0 +1,13 @@
+function longestSlideDown(pyramid) {
+  
+  let temp = pyramid.map(row => row.slice());
+
+  
+  for (let row = temp.length - 2; row >= 0; row--) {
+    for (let col = 0; col < temp[row].length; col++) {
+      
+      temp[row][col] += Math.max(temp[row + 1][col], temp[row + 1][col + 1]);
+    }
+  }
+  return temp[0][0];
+}
