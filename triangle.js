4ab72e385ac4b5fae6910b09442eb4a6fd10e114
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-27 13:47:29 +0200

    27

--- triangle.js
+++ triangle.js
@@ -2,9 +2,9 @@ function towerBuilder(nFloors) {
   let arr = [];
 
   for (let i = 0; i < nFloors; i++) {
-    let spaces = " ".repeat(nFloors - i - 1);
-    let stars = "*".repeat(2 * i + 1);
-    arr.push(spaces + stars + spaces);
+    let j = " ".repeat(nFloors - i - 1);
+    let k= "*".repeat(2 * i + 1);
+    arr.push(j + k+ j);
   }
 
   return arr;
