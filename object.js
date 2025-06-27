803ca51354b627c32d682374b38cbea43c861535
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-27 11:39:22 +0200

    27

---
+++ object.js
@@ -0,0 +1,17 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "EN"
+};
+
+// Add getter property using defineProperty
+Object.defineProperty(person, "fullName", {
+  get: function() {
+    return this.firstName + " " + this.lastName;
+  }
+});
+
+// Get all properties
+var result = Object.getOwnPropertyNames(person);
+console.log(result);           // ['firstName', 'lastName', 'language', 'fullName']
+console.log(person.fullName); // John Doe
