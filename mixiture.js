a28de6ce35cd12a18de55b05dcf99b66745654d6
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-28 23:23:55 +0200

    28

--- mixiture.js
+++ mixiture.js
@@ -1,3 +1,4 @@
+//array to the key:value array
 var number=(array)=> Object.entries(array).map(([key,value])=>[(Number(key)+1)+": "+value]).flat()
 var num=(a)=>a.map((i,e)=>`${++e}: ${i}`)
 var n=[1,2,3,4]
