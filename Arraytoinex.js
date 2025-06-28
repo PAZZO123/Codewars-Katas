453e04b19b3ca2f7169504637031bbb00146a7e7
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-28 23:04:33 +0200

    28

--- Arraytoinex.js
+++ Arraytoinex.js
@@ -3,3 +3,12 @@ var num=(a)=>a.map((i,e)=>`${++e}: ${i}`)
 var n=[1,2,3,4]
 console.log(num(n))
 console.log(number(n))
+//multiply the array
+function countBy(x, n) {
+  var z = [];
+
+  for(let i=x;i<=x*n;i+=x){
+    z.push(i)
+        }
+  return z;
+}
