6878fc02742163918c73b674818432356bb4dcf1
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-29 22:31:43 +0200

    29 change

--- mixture2.js
+++ mixture2.js
@@ -23,3 +23,11 @@ var hero=(bullets, dragons)=>bullets>=dragons*2;
 var getAge=(inputString) => parseInt(inputString);
 console.log(getAge('8 years old'))//8
 
+/*Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  */
+var doubleChar=(str) =>str.split('').map((n)=>n+n).join('')
+
