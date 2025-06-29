cd6f91804dec2bfb776ab7a91859834e9524d552
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-29 11:56:40 +0200

    29

---
+++ hypotenuse.js
@@ -0,0 +1,14 @@
+//to find hypotenus and throw error 6 Kyu
+function calculateHypotenuse(a, b) {
+
+  if (
+    typeof a !== 'number' || typeof b !== 'number' ||
+    !isFinite(a) || !isFinite(b) ||
+    a <= 0 || b <= 0
+  ) {
+    throw new Error("Invalid input");
+  }
+
+  const hypotenuse = Math.sqrt(a ** 2 + b ** 2);
+  return Number(hypotenuse.toFixed(3));
+}
