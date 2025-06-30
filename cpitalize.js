fcf52523672c64c557bda82252c99ba3559e84a7
PAZZO123 <smbabazipatrick@gmail.com>
2025-06-30 16:11:40 +0200

    30

---
+++ cpitalize.js
@@ -0,0 +1,9 @@
+function capitalizeOdd(str){
+    var capital=str.split('').reduce(function(n,m,i){
+        if(i%2===1) return n+m.toUpperCase();
+        return n+m
+        
+    },"")
+    return capital
+}
+console.log(capitalizeOdd("Javascript"))
