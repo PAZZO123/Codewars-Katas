cefcf4152c7326cbf82b5f9808ef98ba4843c791
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-07 13:33:05 +0200

    J7

---
+++ evenIndex.js
@@ -0,0 +1,5 @@
+var  removeEveryOther=(arr)=>arr.filter((n,index)=>index%2===0)
+/*Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]*/
