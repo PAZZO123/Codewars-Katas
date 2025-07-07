b62f29896689b719a5feee846e8d4a6f2eced1bd
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-07 10:15:33 +0200

    last

--- SortOdd.js
+++ SortOdd.js
@@ -1,6 +1,6 @@
 //this function aims to sort only odd element and remain others as they are
 function sortArray(array) {
-  const odds = array.filter(n => n % 2 !== 0).sort((a, b) => a - b);
+  const odds = array.filter(n => n % 2 !== 0).sort((a, b) => a - b)
   
   
   return array.map(n => (n % 2 === 0 ? n : odds.shift()));
