9fb1ff34bf2bd8ca7a90d0e78dab3740a9fa569d
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-07 15:00:10 +0200

    J7

---
+++ SumdigPwer.js
@@ -0,0 +1,15 @@
+function sumDigPow(a, b) {
+  var sumdig=[]
+  for(var num=a; num<=b; num++){
+    var nums=String(num);
+    var digits=nums.split('')
+    var sum=0
+    for(var i=0;i<digits.length;i++){
+      sum+=Number(digits[i]**(i+1))
+      if(num ===sum){
+        sumdig.push(num)
+      }
+    }
+  }
+  return sumdig
+}
