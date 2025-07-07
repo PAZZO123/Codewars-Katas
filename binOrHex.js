f78d78745fe3242a0e5c70be5ffa2b04b5f77b66
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-07 14:30:24 +0200

    J7

---
+++ binOrHex.js
@@ -0,0 +1,7 @@
+/*This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.*/
+
+var evensAndOdds=(num)=>num%2===0?num.toString(2):num.toString(16)
