2a73e5463734016aa8cde5e2af0545474e7c776a
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-08 11:05:55 +0200

    J8

--- binOrHex.js
+++ binOrHex.js
@@ -4,4 +4,5 @@ If a number is even, convert it to binary.
 If a number is odd, convert it to hex.
 Numbers will be positive. The hexadecimal string should be lowercased.*/
 
-var evensAndOdds=(num)=>num%2===0?num.toString(2):num.toString(16)
+var evensAndOdds=(num)=>num%2===0?num.toString(2):num.toString(16);
+console.log(evensAndOdds(9))
