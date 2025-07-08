004c2121d1de2d90966872512588a8c9e57f3433
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-08 13:18:52 +0200

    'J8'

--- returnmin.js
+++ returnmin.js
@@ -23,3 +23,23 @@ function clamp(number, a, b, result) {
   if (number < min) return min;
   if (number > max) return max;
 }
+clamp( 5, 10 )    // -> 5
+clamp(-5, 10 )    // -> 0
+clamp(15, 10 )    // -> 10
+
+clamp( 5, 2, 12 ) // -> 5
+clamp( 0, 2, 12 ) // -> 2
+clamp(15, 2, 12 ) // -> 12
+
+// -- result value provided...
+clamp( 0, 2, 12, 6 )     // -> 6 
+clamp( 0, 2, 12, false ) // -> false
+clamp( 0, 2, 12, "???" ) // -> "???"
+//.. but ...
+clamp( 7, 2, 12, 6 )     // -> 7 ) 
+clamp( 7, 2, 12, false ) // -> 7 } because 7 is in the range [2,12]
+clamp( 7, 2, 12, "???" ) // -> 7 )
+
+// -- result provided by a function
+var mid = function(n,a,b){ return a+(b-a)/2 }
+clamp( 0, 2, 12, mid ) // -> 7 ( = 2+(12-2)/2 )
