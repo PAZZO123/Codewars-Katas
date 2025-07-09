c3e23b275bf773d1c24431c5241a04f920950241
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-09 20:34:25 +0200

    'J9'

--- numName.js
+++ numName.js
@@ -1,2 +1,5 @@
 var switchItUp=(n)=>n===1?'One':n===2?'Two':n===3?'Three':n===4?'Four':n===5?'Five':
                n===6?'Six':n===7?'Seven':n===8?'Eight':n===9?'Nine':'Zero'
+
+//replace
+var removeExclamationMarks=(s) => s.replaceAll("!","");
