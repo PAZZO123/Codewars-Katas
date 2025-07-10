6fd8683cea5ac19a2193e99c2d4f6271f45d84b0
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-10 17:05:10 +0200

    'j10'

---
+++ grid.js
@@ -0,0 +1,25 @@
+function Hero() {
+  this.x = 0; // row
+  this.y = 0; // column
+  this.position = "00"; // string representation
+}
+
+Hero.prototype.move = function (dir) {
+  if (dir === "up") {
+    if (this.x === 0) throw new Error("Out of bounds");
+    this.x--;
+  } else if (dir === "down") {
+    if (this.x === 4) throw new Error("Out of bounds");
+    this.x++;
+  } else if (dir === "left") {
+    if (this.y === 0) throw new Error("Out of bounds");
+    this.y--;
+  } else if (dir === "right") {
+    if (this.y === 4) throw new Error("Out of bounds");
+    this.y++;
+  } else {
+    throw new Error("Invalid direction");
+  }
+
+  this.position = `${this.x}${this.y}`;
+};
