32f3f4d82db232e7b0934c1f6e55048138a8187b
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-11 13:52:20 +0200

    'J11'

--- DogRating.js
+++ DogRating.js
@@ -1,3 +1,4 @@
 var weRateDogs=(str, rating)=>str.slice(0,str.indexOf('/')-1)+""+rating.toString()+str.slice(str.indexOf('/'))
  
 //test
+console.log(weRateDogs('This is Tucker. He would like a hug. 3/10 someone hug him', 11))
