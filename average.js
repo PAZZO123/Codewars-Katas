475f94c88158f716e227f87e364d048d7284daa6
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-11 14:04:15 +0200

    'J11'

--- average.js
+++ average.js
@@ -1,4 +1,7 @@
+//8kyu
 var findAverage=(array)=>array.length===0?0:array.reduce((sum,n)=>(sum+n),0)/array.length
-
 //test
 console.log(findAverage([1,2,3]))//2
+
+//7 kyu version
+var avg=(a)=>a.reduce((n,b)=>n+b,0)/a.length
