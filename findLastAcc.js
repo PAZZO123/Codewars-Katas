1828b6fbf1ff31cd06b9a434edc43bfe4232ad0c
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-12 23:46:30 +0200

    'J12'

--- findLastAcc.js
+++ findLastAcc.js
@@ -1,7 +1,8 @@
-var findAll=(array, n) =>array.reduce((acc,num,idx)=>{
+var findAll=(array, n) =>array.reduce((acc,num,i vcdx)=>{
   if(num===n){
   acc.push(idx)
 }
   return acc;
 },[])
 
+console.log([2,3,4,5,6,7,7],7)
