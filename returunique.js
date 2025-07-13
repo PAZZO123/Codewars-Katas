bfd521d22979c51fdd16970d793265968535070a
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-13 08:29:28 +0200

    'new'

--- returunique.js
+++ returunique.js
@@ -1,5 +1,5 @@
 var findUniq=(arr) =>arr.find(n=>arr.indexOf(n)===arr.lastIndexOf(n))
   var noduplicate=[...new Set(arr)]
-  //return arr.find((item, index) => arr.indexOf(item) === index)
+  //return arr.filter((item, index) => arr.indexOf(item) === index)
   //test
   console.log(findUniq([1,1,2,3,3,2,2,2,1,3]))
