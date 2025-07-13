3d64502348653184ddda2fa00050b0be1ee8e21f
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-13 08:16:48 +0200

    'new'

--- evenIndex.js
+++ evenIndex.js
@@ -3,3 +3,4 @@ var  removeEveryOther=(arr)=>arr.filter((n,index)=>index%2===0)
 
 Example:
 ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]*/
+console.log(removeEveryOther([2,3,5,6,7,8]))
