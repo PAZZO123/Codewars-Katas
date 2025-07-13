f5bbc22935d5a16af18e05034b7f1697a8a72db7
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-13 08:20:56 +0200

    'new'

--- OpenorSenior.js
+++ OpenorSenior.js
@@ -12,3 +12,4 @@ Example
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]*/
 var openOrSenior=(data) => data.map(([age, handicap]) =>  age >= 55 && handicap > 7 ? "Senior" : "Open");
+//test
