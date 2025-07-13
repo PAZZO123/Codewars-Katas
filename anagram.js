f96f42c2bd348e5bb6c863c7a90d859466b40444
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-13 08:11:38 +0200

    'new'

--- anagram.js
+++ anagram.js
@@ -1,4 +1,4 @@
 var isAnagram = (test, original) =>test.toLowerCase().split('').sort().join('')===original.toLowerCase().split('').sort().join('')
 
 //test
-console.log(isAnagram("foefet", "toffee"))
+//console.log(isAnagram("foefet", "toffee"))
