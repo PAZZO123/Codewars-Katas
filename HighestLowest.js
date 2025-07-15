9650eef7d7a9a5e074527bcf2ffee2d5a1fd9aec
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-15 14:45:46 +0200

    'J15'

--- HighestLowest.js
+++ HighestLowest.js
@@ -1,6 +1,7 @@
 var highAndLow=(numbers)=>{
-  var sorted=(numbers.split(' ').map(n=>parseInt(n)).sort((a,b)=>a-b))
-  return [sorted[sorted.length-1],sorted[0]].join(" ") }
+  var sorted=numbers.split(' ').map(n=>parseInt(n))//.sort((a,b)=>a-b))
+  return `${Math.max(...sorted)} ${Math.min(...sorted)}` //[sorted[sorted.length-1],sorted[0]].join(" ") 
+  }
                
 //test
 console.log(highAndLow('5 -7 -9 -10 3 4 5 6 7 8'))
