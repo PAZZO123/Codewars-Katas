e6bab96ed380e5a6ee82d9fe9f55aa3d28f8715d
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-16 10:33:46 +0200

    'J16'

--- FilterUpperIndex.js
+++ FilterUpperIndex.js
@@ -1,3 +1,3 @@
 var capitals = (word)=>word.split('').map((n,idx)=>n<='Z'&& n>='A'?idx:'').filter(n=>n!=='')
 //test
-console.log(capitals('IloVe CodInG'))
+console.log(capitals('IloVe CodInG With My HearT'))
