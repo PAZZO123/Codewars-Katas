2bc0533fa1f01e516098a879893981d33e75d6b1
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-16 15:12:00 +0200

    'J16'

--- SperTest.js
+++ SperTest.js
@@ -1,3 +1,4 @@
 var chromosomeCheck=(sperm)=>sperm.includes('XY')?"Congratulations! You're going to have a son.":
 sperm.includes('XX')?"Congratulations! You're going to have a daughter.":undefined
+//test
   
