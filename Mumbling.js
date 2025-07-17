47b6dd558d3b28f3e21b469a341c12d38b2ab533
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-17 10:44:25 +0200

    'J16'

--- Mumbling.js
+++ Mumbling.js
@@ -1,4 +1,4 @@
 var accum=(s) => s.split('').map((a,idx)=>a.toUpperCase()+a.repeat(idx+1).toLowerCase().slice(1)).join('-')
     
 //test console
-console.log(accum('PATRICK'))//P-Aa-Ttt-Rrrr-Iiiii-Cccccc-Kkkkkkk will be
+console.log(accum('PATRICK'))//P-Aa-Ttt-Rrrr-Iiiii-Cccccc-Kkkkkkk will be logged
