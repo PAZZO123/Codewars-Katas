cb4adfe13cdf02f96e75d0df84b562edfa531305
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-17 15:06:34 +0200

    'J17'

---
+++ BouncingBall.js
@@ -0,0 +1,6 @@
+function bouncingBall(h,  b,  w) {
+  var ratio=w/h
+  var log=Math.log(ratio)/Math.log(b)
+  var result=1+2*Math.floor(log)
+  return w===1 && b<=0.5?1:h>0&&(b>0&&b<1)&&w<h?result:-1
+}
