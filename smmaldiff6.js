dadf860fbeb0947f984752e5e295d1f45ea2f686
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-17 14:33:05 +0200

    'J16'

---
+++ smmaldiff6.js
@@ -0,0 +1,17 @@
+function oneCharDifference(s1, s2) {
+  var k=0
+  var count=0;
+  var maxLength=s1.length>s2.length?s1.length:s2.length
+
+ 
+   for(var i=0;i<maxLength;i++)
+    {
+     if(  s1.split('').includes(s2.charAt(i))){
+       //console.log(s1.charAt(i))
+      count ++ 
+     }
+                  }
+     
+
+  return maxLength===count+1 ||(count===maxLength && (s1.length===s2.length+1 ||s1.length+1===s2.length))
+   }
