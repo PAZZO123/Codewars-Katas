998c82d82924e3fe0844d1b2d492270ffebb6b42
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-17 10:45:41 +0200

    'J16'

--- AlternatingCase.js
+++ AlternatingCase.js
@@ -2,4 +2,4 @@ String.prototype.toAlternatingCase = function () {
   return this.split('').map(n=>n===n.toUpperCase()?n.toLowerCase():n.toUpperCase()).join('')
 }
 //test console
-console.log('Hello Pazzo'.toAlternatingCase())//hELLO pAZZO wii
+console.log('Hello Pazzo'.toAlternatingCase())//hELLO pAZZO will
