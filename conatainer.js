76c07a1ced8b6b8ddc59b1c75949d9b193a8ec48
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-17 10:43:42 +0200

    'J16'

--- conatainer.js
+++ conatainer.js
@@ -1,3 +1,3 @@
 const strCount = (str, letter) => [...str].reduce((count, char) => char === letter ? count + 1 : count, 0);
 // test console
-console.log(strCount('patrick','t'))//1 times
+console.log(strCount('pattrick','t'))//2 times
