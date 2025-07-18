3bc027830d77e1a5ea5939dcf7698979af57a6b6
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-18 23:34:19 +0200

    'J18'

---
+++ smile.js
@@ -0,0 +1,10 @@
+function countSmileys(arr) {
+var valid =[':)',':D',';-D',':~)',';~D',':-)',':-D',';D',";~)",";-)",";)",":~D"]
+var count=0
+  for(var i=0;i<arr.length;i++){
+    if(valid.includes(arr[i])){
+      count++
+    }
+  }
+  return count
+}
