84f6bb21066461d4485eea6e47f38473f73dfb5a
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-20 11:16:31 +0200

    'J20'

--- BreakCamelCase.js
+++ BreakCamelCase.js
@@ -1,4 +1,4 @@
 var solution=(string)=>string.split('').map(n=>n===n.toUpperCase()?' '+n:n).join('')
 
 //test console
-console.log(solution('camelCase'))
+console.log(solution('camelCase'))//camel Case
