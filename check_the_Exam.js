8cf8cd7fcc78c0e35e86a045647e876bf6172f3b
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-21 13:06:51 +0200

    'J21'

--- check_the_Exam.js
+++ check_the_Exam.js
@@ -1,4 +1,4 @@
 var checkExam=(array1, array2) =>Math.max(array1.reduce((c,el,i)=>array2[i]===el?c+=4:array2[i]===''?c+=0:c-=1,0),0)
-
+//test 
 console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))//0
 console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))
