802693cb9b0e42e5c19eb0856955dcbe8aba6829
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-21 13:06:31 +0200

    'J21'

--- check_the_Exam.js
+++ check_the_Exam.js
@@ -1,8 +1,4 @@
 var checkExam=(array1, array2) =>Math.max(array1.reduce((c,el,i)=>array2[i]===el?c+=4:array2[i]===''?c+=0:c-=1,0),0)
-// {
-//  var count=0
-//  for(var i=0;i<array1.length;i++){
-//    array2[i]===""?count+=0:array1[i]===array2[i]?count+=4:count-=1  
-//  }
-//   return count>0?count:0;
-// }
+
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))//0
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))
