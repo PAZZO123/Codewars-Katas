16d0e42bfa22a9983678fd55f4d65d2cb322d5a7
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-21 13:11:55 +0200

    'J21'

--- Add_length.js
+++ Add_length.js
@@ -1,3 +1,3 @@
 var addLength=(str)=>str.split(' ').map(n=>n+" "+n.length)
 //test
-console.log(addLength('Hello I am Patrick'))
+console.log(addLength('Hello I am Patrick'))//[ 'Hello 5', 'I 1', 'am 2', 'Patrick 7' ]
