c11a54a14c53297bd870949e74c8acb85af3c618
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-21 18:59:14 +0200

    'J21'

---
+++ Most_frequently_added_words.js
@@ -0,0 +1,19 @@
+function topThreeWords(text) {
+  if(text==="  '  ")return[]
+ let textArr = text.toLowerCase()
+  .replaceAll('/', ' ')          
+  .replaceAll('\n', ' ')         
+  .replaceAll(',', ' ')          
+  .replaceAll('#', ' ')         
+  .replaceAll("'t", "\'t")
+  .replaceAll(".", '')           
+  .split(' ')                    
+  .filter(n => n !== '');        
+    var seen={}     
+for( let word of textArr ){
+  seen[word]=(seen[word]||0)+1
+}
+ let target=Object.entries(seen).sort((a,b)=>b[1]-a[1]).slice(0,3)
+ return target.map(el=>el[0])
+ 
+}
