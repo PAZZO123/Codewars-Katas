2f5bff7b859c191657278243af3121505e6ace9b
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-21 12:27:36 +0200

    'J21'

--- Merge_two_sorted_arrays.js
+++ Merge_two_sorted_arrays.js
@@ -1,3 +1,3 @@
 var  mergeArrays=(arr1, arr2)=>[...new Set(arr1.concat(arr2).sort((a,b)=>a-b))]
 //test
-console.log(mergeArrays([6,4,4,5,6],[9,0,1,2,3]))
+console.log(mergeArrays([6,4,4,5,6],[9,0,1,2,3]))//[0, 1, 2, 3,4, 5, 6, 9]
