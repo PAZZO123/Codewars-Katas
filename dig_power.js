ebbd2f0995da2c1f89290cf400b20314d037ea70
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-26 20:41:38 +0200

    'J25'

--- dig_power.js
+++ dig_power.js
@@ -2,5 +2,5 @@ function digPow(n, p){
   var result=n.toString().split('').reduce((acc,el,idx)=>acc+Number(el)**(p+idx),0)/n
   return Number.isInteger(result)?result:-1 
 }
-
+//test
 console.log(digPow(89,1))
