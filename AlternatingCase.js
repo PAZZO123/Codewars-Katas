da9bc3b654d8abd0cd27aeb80ea5090281838417
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-27 18:33:36 +0200

    'J27'

--- AlternatingCase.js
+++ AlternatingCase.js
@@ -1,5 +1,5 @@
 String.prototype.toAlternatingCase = function () {
   return this.split('').map(n=>n===n.toUpperCase()?n.toLowerCase():n.toUpperCase()).join('')
 }
-//test console
+//test 
 console.log('Hello Pazzo'.toAlternatingCase())//hELLO pAZZO will be  logged
