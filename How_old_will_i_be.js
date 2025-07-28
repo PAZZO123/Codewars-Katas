724f315f718e7475c5b554b20b45e1423fbe9006
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-28 14:50:50 +0200

    J28

--- How_old_will_i_be.js
+++ How_old_will_i_be.js
@@ -1,7 +1,6 @@
-var  calculateAge=(y1,y2) =>y2>y1?((y2-y1)===1?`You are ${y2-y1} year old.`:`You are ${y2-y1} years old.`):
-y1===y2?`You were born this very year!`:(y1-y2)===1?
-"You will be born in "+ (y1-y2)+" year.":"You will be born in "+ (y1-y2)+" years."
+var  calculateAge=(y1,y2) =>y2>y1?`You are ${y2-y1} year${y2-y1>1?'s':''} old.`:y1===y2?`You were born this very year!`:
+`You will be born in ${y1-y2} year${y1-y2>1?'s':''}.`
 
 
 //test 
-console.log(calculateAge(2002,2025))//You are 23 years old.
+console.log(calculateAge(2002,2001))//You are 23 years old.
