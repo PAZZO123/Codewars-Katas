ccbd5d95d65b602e9f8f13e733552334860ed693
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-28 14:42:59 +0200

    J28

---
+++ How_old_will_i_be.js
@@ -0,0 +1,5 @@
+var  calculateAge=(y1,y2) =>y2>y1?((y2-y1)===1?`You are ${y2-y1} year old.`:`You are ${y2-y1} years old.`):
+y1===y2?`You were born this very year!`:(y1-y2)===1?
+"You will be born in "+ (y1-y2)+" year.":"You will be born in "+ (y1-y2)+" years."
+
+
