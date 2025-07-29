6766d322869c472b1e069acfa1b63624c8a12381
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-29 13:38:15 +0200

    The Coupon Code

--- The_coupon_code.js
+++ The_coupon_code.js
@@ -1,3 +1,3 @@
 const checkCoupon=(eCode, cCode, cDate, eDate)=>eCode===cCode && Date.parse(cDate)<=Date.parse(eDate)
-console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014'))
-console.log(checkCoupon('123a','123','September 5, 2014','October 1, 2014'))
+console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014'))//true
+console.log(checkCoupon('123a','123','September 5, 2014','October 1, 2014'))//true
