2a75ce404d2534cd2c3c669a087ce5c2cf501bc5
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-29 13:59:33 +0200

    Flatting and Sorting

--- Flatten_and_sort_an_array.js
+++ Flatten_and_sort_an_array.js
@@ -2,4 +2,4 @@
 
 const flattenAndSort=(array)=>array.flat().sort((a,b)=>a-b)
   //test
-  console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]))
+  console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]))//[1, 2, 3, 4, 5, 6, 100]
