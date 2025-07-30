0098619dfacef6d94ff15039b46a8d468da92072
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-30 14:54:54 +0200

    Url anchor removal kata

--- Removing_anchor_from_url.js
+++ Removing_anchor_from_url.js
@@ -2,3 +2,4 @@ const removeUrlAnchor=(url)=>url.includes('#')?url.slice(0,url.indexOf('#')):url
   
 //test console
 console.log(removeUrlAnchor('www.codewars.com#about'))//www.codewars.com
+console.log(removeUrlAnchor('www.codewars.com/katas/'))//www.codewars.com/katas/
