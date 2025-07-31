1a11382602b32401b1f8c99a2d308e9293459c9c
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-31 16:16:47 +0200

    feat:What a day kata

---
+++ return_the_day.js
@@ -0,0 +1,4 @@
+function whatday(num) { 
+var days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+return num>=1 && num<=7 ?days[num-1]:`Wrong, please enter a number between 1 and 7`
+}
