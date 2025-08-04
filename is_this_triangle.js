558649865bf30420e6aaf41ac90ce1eccfc7d4a4
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-04 14:44:22 +0200

    Feat: Is this triangle kata 7kyu

--- is_this_triangle.js
+++ is_this_triangle.js
@@ -1,1 +1,7 @@
 const isTriangle =(a,b,c)=>a+b>c&&b+c>a&&c+a>b
+
+//test cases
+console.log(isTriangle(3, 4, 5)); // true
+console.log(isTriangle(1, 2, 3)); // false
+console.log(isTriangle(5, 5, 5)); // true
+console.log(isTriangle(0, 0, 0)); // false
