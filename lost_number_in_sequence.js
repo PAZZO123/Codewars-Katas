8dd0a3c9814669dd71054188a1eb88ae8f898cca
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-04 15:07:22 +0200

    Feat: Find deleted number kata 7kyu

--- lost_number_in_sequence.js
+++ lost_number_in_sequence.js
@@ -1,2 +1,7 @@
 const  findDeletedNumber=(arr, mixArr) =>arr.find((el)=>!mixArr.includes(el))||0
+
+//test cases
+console.log(findDeletedNumber([1, 2, 3], [1, 2])); // 3
+console.log(findDeletedNumber([1, 2, 3], [1, 2, 3])); // 0
+
   
