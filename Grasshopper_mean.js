c08809e53e86e5ba893658945d1f0da5b4b92d8a
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-04 14:45:06 +0200

    Feat: GrassHoper mean kata 8kyu

--- Grasshopper_mean.js
+++ Grasshopper_mean.js
@@ -1,2 +1,6 @@
 const findAverage=(nums)=> nums.reduce((acc,el)=>acc+el,0)/nums.length
   
+//test cases
+console.log(findAverage([1, 2, 3, 4, 5])); // 3
+console.log(findAverage([10, 20, 30])); // 20
+console.log(findAverage([5, 15, 25, 35]));
