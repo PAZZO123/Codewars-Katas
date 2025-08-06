cefddf30c2f1264511034e07697bb90c730447c2
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-06 15:13:38 +0200

    A6

---
+++ The_maximum_and_minimum_difference6k.js
@@ -0,0 +1,12 @@
+function maxAndMin(arr1,arr2){
+  var diff=0
+  var result=[]
+  for(let el of arr2){
+    for(let el2 of arr1){
+      
+      diff=el-el2;
+      result.push(Math.abs(diff))
+    }
+  }
+  return[Math.max(...result),Math.min(...result)]
+}
--- find_the_middle_element.js
+++ find_the_middle_element.js
@@ -1,2 +1,4 @@
 const gimme = arr => [...arr].indexOf(arr.sort((a,b) => a-b)[1]);
 
+//test cases
+console.log(gimme([2,4,3]))//2
