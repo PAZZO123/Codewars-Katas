bd6ab21f3c39d877e03f711a30f5f5be57c40ffe
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-07 14:50:03 +0200

    A7

--- Binary_coded_decimal.js
+++ Binary_coded_decimal.js
@@ -2,3 +2,4 @@ function toBcd(n){
   return( n>=0? n.toString().split('').map(n=>Number(n).toString(2).padStart(4,'0')).join(' '): 
          "-"+Math.abs(n).toString().split('').map(n=>Number(n).toString(2).padStart(4,'0')).join(' '))
 }
+//test cases
