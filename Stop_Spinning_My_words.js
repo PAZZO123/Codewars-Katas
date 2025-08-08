1bc239a1d424ff48c187a64b3e3ff4bf6121e0b6
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-08 14:49:36 +0200

    A8

---
+++ Stop_Spinning_My_words.js
@@ -0,0 +1,8 @@
+//split string by space to make array of words\
+//map that array check the length of each word
+//if length is 5 or higher reverse the word
+//join the array by the space
+
+const spinWords =(string)=>string.split(' ').map((n)=>n.length>=5?n.split('').reverse().join(''):n).join(' ')
+
+//test cases
