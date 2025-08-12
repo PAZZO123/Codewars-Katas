dde61ef9e7ef4f5d70134c3e1688c5502eaf0d64
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-12 14:56:51 +0200

    feat:add this file

---
+++ create_phone_number_6kyu.js
@@ -0,0 +1,5 @@
+//use template literals
+//use slice method to slice each portion you need 
+//for every slice join by empty string use join()
+
+const createPhoneNumber=(n)=>`(${n.slice(0,3).join('')}) ${n.slice(3,6).join('')}-${n.slice(6).join('')}` 
