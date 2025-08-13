d4f2ab4288c83233ea41ddcfd561ab4e57b81f2a
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-13 19:39:28 +0200

    feat:adding this file

---
+++ Duplicate_Encoder.js
@@ -0,0 +1,5 @@
+function duplicateEncode(word){
+  let words=word.toLowerCase().split('')
+  const duplicate=words.filter((el,i)=>words.indexOf(el)!==i)
+    return  words.map((el,i)=>duplicate.includes(el)?words[i]=")":words[i]='(').join('')
+}
