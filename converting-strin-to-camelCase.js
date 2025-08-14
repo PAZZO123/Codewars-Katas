26de5850b8ea0f5f39899fa8e3e026a1d8e4fa7a
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-14 21:55:52 +0200

    feat:adding this kata 6kyu

--- converting-strin-to-camelCase.js
+++ converting-strin-to-camelCase.js
@@ -1,6 +1,6 @@
 let toCamelCase=(s)=>s.split('_').join(' ').split('-').join(' ').split(' ').map((n,i)=>i===0?n:
 n.charAt(0).toUpperCase()+n.slice(1)).join('')
 //test cases
-
+console.log(toCamelCase('camel-case'))//camelCase
 
 
