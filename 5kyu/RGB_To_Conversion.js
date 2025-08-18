5f94dfd45924188263b419f4692c88ec6ea84bfc
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-18 23:00:20 +0200

    feat:adding this kata 5 kyu

---
+++ 5kyu/RGB_To_Conversion.js
@@ -0,0 +1,7 @@
+function rgb(r, g, b) {
+  let red=r<0?"00":r<255?r.toString(16).padStart(2,'0').toUpperCase():"FF"
+  let blue=b<0?"00":b<255?b.toString(16).padStart(2,'0').toUpperCase():"FF"
+  let green=g<0?"00":g<255?g.toString(16).padStart(2,'0').toUpperCase():"FF"
+
+    return `${red}${green}${blue}`
+}
