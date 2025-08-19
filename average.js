b5c279b3846d2dcc2fb0ca9a48d5dd82aca233da
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-19 11:34:42 +0200

    feat:adding this from Udacity

--- average.js
+++ average.js
@@ -7,3 +7,12 @@ console.log(findAverage([1,2,3]))//2
 var avg=(a)=>a.reduce((n,b)=>n+b,0)/a.length
 
 //average using rest parameters
+function average(...numbers) {
+    return numbers.length?numbers.reduce((acc,n)=>acc+n,0)/numbers.length:0
+    
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
