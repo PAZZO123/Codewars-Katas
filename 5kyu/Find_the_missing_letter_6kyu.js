f05b36e272aaa38cb1905c77040fe284cfd3df89
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-26 21:05:37 +0200

    feat:adding this 5kyu kata

---
+++ 5kyu/Find_the_missing_letter_6kyu.js
@@ -0,0 +1,19 @@
+function findMissingLetter(arr) {
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const lower = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  const sequence = arr[0] === arr[0].toUpperCase() ? upper : lower;
+
+  const start = sequence.indexOf(arr[0]);
+  const end = sequence.indexOf(arr[arr.length - 1]);
+
+  for (let i = start; i <= end; i++) {
+    if (!arr.includes(sequence[i])) {
+      return sequence[i];
+    }
+  }
+
+  return null; 
+}
+
+console.log(findMissingLetter(['o','q','r','s'])); 
