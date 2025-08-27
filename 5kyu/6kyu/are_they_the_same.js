029ea5d850bd35d09adb489de8fe18c2ddbbdd4a
PAZZO123 <smbabazipatrick@gmail.com>
2025-08-27 20:00:23 +0200

    feat:adding this 5kyu kata

--- 5kyu/6kyu/are_they_the_same.js
+++ 5kyu/6kyu/are_they_the_same.js
@@ -3,3 +3,6 @@ function comp(a, b) {
   return a.map(x => x*x).sort().join() == b.sort().join();
 }
 //test cases
+ let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+ let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+console.log(comp())
