585c565e289d94971f189f192d90a7d9d77a6799
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-02 12:51:55 +0200

    adding this kata

---
+++ 5kyu/6kyu/Give_me_data.js
@@ -0,0 +1,7 @@
+function hasPermission(db, cat) {
+  if (db.includes(`${cat}_deny`)) return false
+  if (db.includes(`${cat}_allow`)) return true
+  if (db.includes('*_deny')) return false
+  if (db.includes('*_allow')) return true
+  return false
+}
