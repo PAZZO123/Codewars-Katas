8f453708ebfcf94f86fcdc17c200fa5f7ce61258
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-02 12:50:34 +0200

    adding this kata

--- 5kyu/6kyu/Count characters_in_your_string.js
+++ 5kyu/6kyu/Count characters_in_your_string.js
@@ -11,4 +11,4 @@ function counts(str) {
    return (m = {}, str.split('').forEach((c) => m[c] = (m[c] || 0) + 1), m);
 }
 //test cases
-console.log(count('patrick'))
+console.log(count('patrick'))//{ p: 1, a: 1, t: 1, r: 1, i: 1, c: 1, k: 1 }
