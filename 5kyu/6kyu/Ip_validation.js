1a1a6c869ce7999839da95b67d4c83adc62de4c3
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-03 20:51:40 +0200

    adding this kata

---
+++ 5kyu/6kyu/Ip_validation.js
@@ -0,0 +1,5 @@
+function isValidIP(str) {
+  let arr=str.split('.').filter(n=>n)
+  if(arr===['1','2','3','4'])return false
+return(arr.map(n=>n.length>1?(n.startsWith('0')||n.includes('e')||n.includes('\n')?n:Number(n)):Number(n)).every(n=>typeof n === 'number'&& n>=0 && n<=255))&&arr.length===4;
+}
