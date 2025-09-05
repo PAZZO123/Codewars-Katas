f4783c5b39f510182d828ff67f518fe53a9feff0
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-05 13:13:22 +0200

    Adding this kata

--- 6kyu/Strings_strings_strings(HARD).js
+++ 6kyu/Strings_strings_strings(HARD).js
@@ -1,3 +1,6 @@
 Array.prototype.toString = function() {
   return "[" + this.map(v => typeof v  === "string" ? `'${v}'` : v).join(",") + "]";
+
 }
+//test cases
+console.log([123,'pazzo'].toString())
