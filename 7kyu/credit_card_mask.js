2c8228c6d36e58a946ee8e8b25b5585caedd1319
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-08 21:56:36 +0200

    Feat:adding this kata

--- 7kyu/credit_card_mask.js
+++ 7kyu/credit_card_mask.js
@@ -4,4 +4,4 @@ function maskify(cc) {
 return cc.slice(0,cc.length-4).split('').map(n=>'#').join('')+cc.slice(cc.length-4)
 }
 //test cases
-console.log(maskify('Tetcases'))
+console.log(maskify('Tetcases'))//####ases
