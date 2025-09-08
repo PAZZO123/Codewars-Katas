a09c29f326aecef4a09d7ea8f44145ca2b8e9c52
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-08 21:47:05 +0200

    Feat:adding this kata

--- 7kyu/remove_the_minimum.js
+++ 7kyu/remove_the_minimum.js
@@ -1,6 +1,7 @@
 function removeSmallest(numbers) {
   const min=Math.min(...numbers)
   return numbers.filter((n,i,arr)=>i!==arr.indexOf(min))
+    // return new Set(str.toLowerCase()).size == str.length;
 }
 //test cases
 console.log(removeSmallest([1,2,3,4,6]))//1
