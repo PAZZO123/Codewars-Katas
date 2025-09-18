1aca5054b74e2347bfc8d37ee3dc64b74d2b9eaa
PAZZO123 <smbabazipatrick@gmail.com>
2025-09-18 20:01:41 +0200

    feat:adding this kata

---
+++ 5kyu/Bit_calculator.js
@@ -0,0 +1,8 @@
+function calculate(num1, num2) {
+  //convert each string representation to An array
+  //reverse an array to power index to 2
+  //map reversed array and map each element by X2**index and then reduce to get sum
+  //Add and return the result of two numbers
+  const toDecimal=(num)=>num.split('').reverse().map((n,i)=>+n*2**i).reduce((acc,el)=>acc+el,0)
+    return toDecimal(num1)+toDecimal(num2)
+}
