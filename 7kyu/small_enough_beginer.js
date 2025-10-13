f7d8f15f932dae87c9d1940213899bd8b5a5671a
PAZZO123 <smbabazipatrick@gmail.com>
2025-10-13 17:29:28 +0200

    adding this file

--- 7kyu/small_enough_beginer.js
+++ 7kyu/small_enough_beginer.js
@@ -2,3 +2,4 @@ const smallEnough=(a, limit)=>a.every(n=>n<=limit)
 //test cases
 console.log(smallEnough([1,2,3,4,5,5,6],3))
 console.log(smallEnough([1,2,3,4,5,5,6],4))
+console.log(smallEnough([1,2,3,4,5,5,6],4))
