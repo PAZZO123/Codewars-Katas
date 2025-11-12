806d44bbc2a36622416b2d3fb266b7ebd499d410
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-12 13:30:49 +0200

    adding this kata

---
+++ 7kyu/Letterbox-paint-squad.js
@@ -0,0 +1,8 @@
+var paintLetterboxes = function(start, end) {
+  let result=Array(10).fill(0)
+  for(let i =start; i<=end;i++){
+    [...String(i)].forEach((n)=>result[n]++)
+  }
+  return result
+}
+// test cases
