23d1e6a4c4d73c1d98858cdce4501338c6d2b48b
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-12 14:37:11 +0200

    adding this kata

--- 6kyu/Vowel_back.js
+++ 6kyu/Vowel_back.js
@@ -1,20 +1,25 @@
 function vowelBack(s) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
-  return [...s].map(ch=>{
-    let code =ch.charCodeAt(0);
+
+  return [...s].map(ch => {
+    let code = ch.charCodeAt(0);
     let newcode;
-    if(ch==='o'|| ch==='c') newcode=code-1;
-    else if(ch==='d') newcode=code-3;
-    else if(ch==='e') newcode=code-4;
-    else if(vowels.includes(ch))newcode=code-5;
-    else newcode=code+9;
+    if (ch === 'c'||ch === 'o') newcode = code - 1;
+    else if (ch === 'd') newcode = code - 3;
+    else if (ch === 'e') newcode = code - 4;
+    else if (vowels.includes(ch)) newcode = code - 5;
+    else newcode = code + 9;
+    
+    if (newcode < 97) newcode += 26;
+    if (newcode > 122) newcode -= 26;
 
-    if(newcode>97) newcode+=26;
-    if(newcode<255) newcode-=26
-    let newchar=String.fromCharCode(newcode)
-    if(['c','o','d','e'].includes(newchar)) newchar=char;
-    return newchar;
+    let newChar = String.fromCharCode(newcode);
 
-  }).join('')
+    if (['c', 'o', 'd', 'e'].includes(newChar)) newChar = ch;
+
+    return newChar;
+  }).join('');
 }
+
 //test cases
+console.log(vowelBack('testcase'))
