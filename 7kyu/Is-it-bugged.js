1e4e165e7e53fb04f59bb34c8abfc34415af3941
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-14 14:38:35 +0200

    adding this kata

---
+++ 7kyu/Is-it-bugged.js
@@ -0,0 +1,11 @@
+function isItBugged(code) {
+  let [d, t] = code.split(' ');
+
+  if (!d || !t) return false;
+  let date = d.split("-");
+  if (date.length !== 3 || date.some(part => part.length < 2)|| +date[2]<1000 || date[1].length>2 ) return false;
+  let time = t.split(":");
+  if (time.length !== 2 || time.some(part => part.length < 2)) return false;
+
+  return true;
+}
