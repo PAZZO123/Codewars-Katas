930772f95cfbe09c90754a2cbbaa1dc909478246
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-19 15:48:17 +0200

    Adding this kata

---
+++ 7kyu/Javascriptasync/nested_chainig.js
@@ -0,0 +1,14 @@
+function getJSON(url){
+    return fetch(url)
+}
+
+getJSON("data1.json")
+  .then((res1) => {
+   console.log(res1)
+  })
+  .catch((err) => {
+    console.log("Caught inside!", err.message);
+    return getJSON("data2.json"); 
+  })
+  .then((res2) => console.log("Recovered:", res2))
+  .catch((err) => console.log("Final error:", err.message)); 
