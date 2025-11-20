b926bfbf7d9c4e8122631e5a69a9c71c04473146
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-20 22:16:10 +0200

    Adding this kata

--- 6kyu/find_within_array.js
+++ 6kyu/find_within_array.js
@@ -7,3 +7,5 @@ function findInArray(array, iterator) {
   return -1;
 }
 //test cases
+var trueIfEven = function(value, index) { return (value % 2 === 0) };
+console.log(findInArray([1,3,4,5,6,7,8],trueIfEven))
