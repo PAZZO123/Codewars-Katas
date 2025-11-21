8be2f2b46c082133b3a1e39a1be922e93d6eeb82
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-21 16:22:50 +0200

    Adding this kata

---
+++ 7kyu/Javascriptasync/function.js
@@ -0,0 +1,9 @@
+console.log(num)//undefined
+GetName();//Hello, Justine
+//console.log(same)//same is not defined
+console.log(GetName)//[Function: GetName]
+
+function GetName(){
+    console.log("Hello , Justine")
+}
+var num=8;
