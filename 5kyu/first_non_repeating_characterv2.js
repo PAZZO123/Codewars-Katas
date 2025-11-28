3e16972beb94fbdafc05d5cc03c1856a4c5042d2
PAZZO123 <smbabazipatrick@gmail.com>
2025-11-28 16:02:06 +0200

    adding this from async and await

--- 5kyu/first_non_repeating_characterv2.js
+++ 5kyu/first_non_repeating_characterv2.js
@@ -2,3 +2,5 @@ function firstNonRepeatingLetter(s) {
   let a=[...s.toLowerCase()]
   return s.charAt(a.indexOf(a.find((n,i)=>a.indexOf(n)===a.lastIndexOf(n))))||''
 }
+//test cases
+console.log(firstNonRepeatingLetter('Totos'))//->s
