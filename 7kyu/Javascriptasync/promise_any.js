99428c7748fbbaab973dd45892b9afa93ee28723
PAZZO123 <smbabazipatrick@gmail.com>
2025-12-02 23:02:19 +0200

    Adding Promise

--- 7kyu/Javascriptasync/promise_any.js
+++ 7kyu/Javascriptasync/promise_any.js
@@ -7,7 +7,7 @@ const myPromise2 = new Promise((resolve, reject) => {
   setTimeout(resolve, 100, "Queen");
 });
 
-// Settle All
+// Any in action 
 Promise.any([myPromise1, myPromise2]).then((results) =>
    console.log(results)//Queen
    
