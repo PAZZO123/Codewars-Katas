99dca22f3bdd75555c9c661439847991f15f50c6
PAZZO123 <smbabazipatrick@gmail.com>
2025-12-02 22:58:13 +0200

    Adding Promise

---
+++ 7kyu/Javascriptasync/promise_race.js
@@ -0,0 +1,13 @@
+const myPromise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 200, "success");
+});
+
+// Create another Promise
+const myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 100, "Error2");
+});
+
+// race 
+Promise.race([myPromise1, myPromise2])
+  .then(result => console.log("Resolved:", result))
+  .catch(error => console.log("Rejected:", error));//Rejected Error2
