2371df460137e7e2ad29d951269a1656b66250db
PAZZO123 <smbabazipatrick@gmail.com>
2025-12-09 18:43:34 +0200

    adding this |

---
+++ 7kyu/Javascriptasync/any.js
@@ -0,0 +1,9 @@
+const promises = [
+Promise.reject(new Error('failure #1')),
+Promise.reject(new Error('failure #2')),
+Promise.reject(new Error('failure #3'))
+];
+Promise.any(promises).then(
+(result) => {console.log(result)},
+(error) => {console.log(error)}
+)
