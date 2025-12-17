83872828a5c8be9a9765ea2e8d159c8e390bab14
PAZZO123 <smbabazipatrick@gmail.com>
2025-12-17 16:46:03 +0200

    Adding this to the Github

--- 7kyu/Mental/Exerccises5.js
+++ 7kyu/Mental/Exerccises5.js
@@ -2,12 +2,14 @@
 const { XMLHttpRequest } = require("xmlhttprequest");
 function fetchToDoWithHeaders() {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://jsonplaceholder.typicode.com/todos/1");
+  xhr.open("POST", "https://jsonplaceholder.typicode.com/todos");
 
   xhr.setRequestHeader("User-Agent", "MyCustomAgent/1.0");
   xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onload = () => {
+      console.log("Response Headers:");
+  console.log(xhr.getResponseHeader('Content-Type'));
     if (xhr.status >= 200 && xhr.status < 300) {
       const parsed = JSON.parse(xhr.responseText);
       console.log("Parsed Response:", parsed);
@@ -15,7 +17,7 @@ function fetchToDoWithHeaders() {
       console.log("Server returned status:", xhr.status);
     }
   };
-
+ console.log(xhr.getAllResponseHeaders())
   xhr.onerror = () => {
     console.log("Network error");
   };
---
+++ 7kyu/Mental/fetchversion5.js
@@ -0,0 +1,21 @@
+const payload={
+    name:'Patrick',
+    date:Date.now(),
+    done: false,
+    message:"Wait Patiently"
+}
+
+function sendData(url,data){
+    return fetch(url,{
+        method:'POST',
+        headers:{'Content-Type':'application/json',
+                  'User-Agent':'Userdefined/V1'
+               },
+        body:JSON.stringify(data)
+    })
+
+}
+sendData('https://jsonplaceholder.typicode.com/todos', payload)
+.then(res=>res.json())
+.then(res=>console.log('We Received ',res))
+.catch(err=>console.log(err.name))
