f2f2cc754a7977732bed20285f93214c6d3bebee
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-25 18:19:11 +0200

    Adding this for Promise API

--- 7kyu/Javascriptasync/promise_any.js
+++ 7kyu/Javascriptasync/promise_any.js
@@ -61,4 +61,4 @@
        },5000)
  })
  Promise.any([p1,p2,p3]).then(
-     result=>console.log(result)).catch(err=>console.log(err))
+     result=>console.log(result)).catch(err=>console.log([...err.errors]))
