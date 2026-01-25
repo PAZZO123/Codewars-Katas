c0395c352d0c17b9b3f2bf41ced10b0857fe5aff
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-25 17:48:43 +0200

    Adding this for Promise chainning )

--- 7kyu/Javascriptasync/Promise_all.js
+++ 7kyu/Javascriptasync/Promise_all.js
@@ -1,13 +1,36 @@
-const myPromise1 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 200, "King");
-});
+// const myPromise1 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 200, "King");
+// });
 
-// Create another Promise
-const myPromise2 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "Queen");
-});
+// // Create another Promise
+// const myPromise2 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, "Queen");
+// });
 
-// Settle All
-Promise.all([myPromise1, myPromise2]).then((results) =>
-   console.log(results)//->[ 'King', 'Queen' ]
-);
+// // Settle All
+// Promise.all([myPromise1, myPromise2]).then((results) =>
+//    console.log(results)//->[ 'King', 'Queen' ]
+// );
+ let p1=new Promise(function(resolve, reject){
+     let decision =true
+     if(!decision){
+         reject('Failure one')
+     }
+     resolve('Success one')
+ })
+  let p2=new Promise(function(resolve, reject){
+     let decision =false
+     if(!decision){
+         reject('Failure two')
+     }
+     resolve('Success two')
+ })
+  let p3=new Promise(function(resolve, reject){
+     let decision =true
+     if(!decision){
+         reject('Failure three')
+     }
+     resolve('Success three')
+ })
+ Promise.all([p1,p2,p3]).then(
+     result=>console.log(result)).catch(err=>console.log(err))
