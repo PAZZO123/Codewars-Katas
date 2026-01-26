a281c0306b393b725f26e0e08dc991b6bcd6f74d
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-26 19:14:26 +0200

    adding this stuff

--- 7kyu/Javascriptasync/fetchtry.js
+++ 7kyu/Javascriptasync/fetchtry.js
@@ -10,14 +10,22 @@
 // }
 // fetchData()
 
-//fetching
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => {
-    if (!res.ok) throw new Error('Unable to fetch')
+//2. fetching
+const url='https://jsonplaceholder.typicode.com/users/10'
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res => {
+//     if (!res.ok) throw new Error('Unable to fetch')
     
-    console.log(res.status)
-    return res.json()
-  })
-  .then(data => console.log(data))
-  .catch(err => console.log(err.message))
+//     console.log(res.status)
+//     return res.json()
+//   })
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err.message))
+
+//3. async and await 
+async function getData(url){
+    let res=await fetch(url)
+    let jsonValue=await res.json()
+    console.log(jsonValue)
+}
+getData(url).catch(err=>console.warn((err.message).toUpperCase()))
