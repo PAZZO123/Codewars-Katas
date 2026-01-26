337afa0b792409688cafc14f201fbca5f48d21bb
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-26 19:04:27 +0200

    adding this

--- 7kyu/Javascriptasync/fetchtry.js
+++ 7kyu/Javascriptasync/fetchtry.js
@@ -1,11 +1,23 @@
-async function fetchData(){
-    try{
-        let data=await fetch('https://dummyurl')
-    let jasonValue=data.json()
-    console.log(data)
-    }
-    catch(err){
-        console.error(err.message)
-    }
-}
-fetchData()
+// async function fetchData(){
+//     try{
+//         let data=await fetch('https://dummyurl')
+//     let jasonValue=data.json()
+//     console.log(data)
+//     }
+//     catch(err){
+//         console.error(err.message)
+//     }
+// }
+// fetchData()
+
+//fetching
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => {
+    if (!res.ok) throw new Error('Unable to fetch')
+    
+    console.log(res.status)
+    return res.json()
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err.message))
