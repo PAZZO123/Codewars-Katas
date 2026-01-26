2783d206561d34c55c4e08c9f339e09077e18be8
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-26 21:09:33 +0200

    adding this

--- 7kyu/Javascriptasync/fetchtry.js
+++ 7kyu/Javascriptasync/fetchtry.js
@@ -38,22 +38,60 @@
 //let url ='http://127.0.0.1:5500/local-sample.json?attempt=123&other=hello'
 
 
-function fetchData(url){
-    let str=new URL(url)
-    //console.log(str.host, str.origin, str.protocol, str.hostname, str.pathname)
-    //fetch(str).then(res=>console.log(res.status))
-    const request=new Request(str,{
-        headers:{'x-steve':'hello'},
-        method:'GET',
-        cache:'no-store'
-    })
-    fetch(request)
-    .then(res=>{
-        if(!res.ok) throw new Error("Invalid Request")
-            console.log(res.status)
-            return res.json()
-    })
-    .then(data=>console.log(data))
-    .catch(err=>console.log(err.message))
-}
-fetchData(url)
+// function fetchData(url){
+//     let str=new URL(url)
+//     //console.log(str.host, str.origin, str.protocol, str.hostname, str.pathname)
+//     //fetch(str).then(res=>console.log(res.status))
+//     const request=new Request(str,{
+//         headers:{'x-steve':'hello'},
+//         method:'GET',
+//         cache:'no-store'
+//     })
+//     fetch(request)
+//     .then(res=>{
+//         if(!res.ok) throw new Error("Invalid Request")
+//             console.log(res.status)
+//             return res.json()
+//     })
+//     .then(data=>console.log(data))
+//     .catch(err=>console.log(err.message))
+// }
+// fetchData(url)
+
+//5. Response
+
+// let obj={
+//     id:crypto.randomUUID(),
+//     name:'the one who knocks',
+//     favColor:'Blue'
+
+// }
+// const imagestr='http://picsum.photos/id/237/300/200';
+// function getData(){
+//     let jsonValue=JSON.stringify(obj)
+//     //console.log(jsonValue)
+//     let file=new File([jsonValue],'mydata.json',{type:'application/json'});
+//     let response= new Response(file,{
+//         status:200,
+//         statusText:'say my name',
+//         headers:{
+//             'content-type': 'application/json',
+//             'content-length':file.size
+//         },
+//     })
+//     //console.log(response.headers.get('content-type'))
+//     fetch(imagestr)
+//     .then(res=>{
+//         if(!res.ok) throw new Error('Invalid')
+//             console.log(res.status)
+//         return res.blob()
+//     })
+//     .then(blob=>{console.log(blob)
+//     let url=URL.createObjectURL(blob)
+//     let img=document.getElementById('img')
+//     img.src=url})
+// }
+// getData()
+//blob binary large object research then
+
+//6. generator
