cb407599927b1411470087523142b891e7ef4b00
PAZZO123 <smbabazipatrick@gmail.com>
2026-01-27 21:54:19 +0200

    adding this

--- 7kyu/Mental/igicupuri3.js
+++ 7kyu/Mental/igicupuri3.js
@@ -4,10 +4,16 @@ Implement a timeout for an asynchronous fetch request. If the request takes long
 https://jsonplaceholder.typicode.com/users
 
 */
-let controller=new AbortController()
-let signal=controller.signal
 
-function fetchData(url){
+
+function fetchData(url,ms){
+ let controller=new AbortController()
+let signal=controller.signal
+//start timer to see
+setTimeout(function(){
+    controller.abort()
+},ms)
+
 return fetch(url,{signal})
 .then(res=>{
 if(!res.ok) throw new Error('Fetch Failed!')
@@ -19,7 +25,4 @@ if(!res.ok) throw new Error('Fetch Failed!')
 })
 }
 
-fetchData('https://jsonplaceholder.typicode.com/users')
-setTimeout(function(){
-    controller.abort()
-},20)
+fetchData('https://jsonplaceholder.typicode.com/users',5000)
