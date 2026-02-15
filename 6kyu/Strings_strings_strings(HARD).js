2561478983d8bfa88f357a8f1ad1de2848f7aaef
PAZZO123 <smbabazipatrick@gmail.com>
2026-02-15 11:55:41 +0200

    Adding this to GitHub

--- 6kyu/Strings_strings_strings(HARD).js
+++ 6kyu/Strings_strings_strings(HARD).js
@@ -3,4 +3,4 @@ Array.prototype.toString = function() {
 
 }
 //test cases
-console.log([123,'pazzo'].toString())//"[123,'pazzo']" will  be 
+console.log([123,'pazzo'].toString())//"[123,'pazzo']" will  be  logged
